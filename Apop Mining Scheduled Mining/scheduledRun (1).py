04e0388646411fb7db68d9a58317bcfe309280b7
apop274 <112342983+apop274@users.noreply.github.com>
2024-04-01 12:54:13 -0700

    Update scheduledRun (1).py

--- Apop Mining Scheduled Mining/scheduledRun (1).py
+++ Apop Mining Scheduled Mining/scheduledRun (1).py
@@ -1,4 +1,4 @@
-#this program opens lolMiner at the sunny times, for the mining to occur at the sunny times
+#this program opens lolMiner or any mining software at the sunny times, for the mining to occur at the sunny times
 
 import subprocess
 import schedule
@@ -7,15 +7,15 @@ import os
 
 def open_app():
    
-   # app_path = r'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk'
     app_path = r'C:\Users\Xxapo\OneDrive\Desktop\Nexa coin\lolMiner_v1.77b_Win64\1.77b\mine_nexa'
     subprocess.Popen(['start', 'cmd', '/c', app_path], shell=True)
 
 def close_app():
     
-    #app_name = 'chrome.exe'
+
     #app_name = 'cmd.exe'
     #app_name = 'lolMiner.exe'
+   
     subprocess.Popen(['taskkill', '/f', '/im', 'lolMiner.exe'], shell=True)
     subprocess.Popen(['taskkill', '/f', '/im', 'cmd.exe'], shell=True)
     #os.system(f'taskkill /f /im {app_name}')
